e866478f131fdebbc75dcbbc11a944de92a49a42
borsha06 <tasfia.sharmin1994@gmail.com>
2018-10-06 22:56:40 +0600

    pushing to heroku

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const
   express = require('express'),
   serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
+  port = process.env.PORT || 5000,
+  path = require('path')
 
 const app = express()
 
 app.use(history())
-app.use(serveStatic(path.join((__dirname + '/dist/spa-mat'))
+app.use(serveStatic(path.join(__dirname, '/dist/pwa-mat')))
 app.listen(port)
